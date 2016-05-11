83d8651341220d7e360ba9d667933feace216758
James Laverty <jlaverty@myseneca.ca>
2016-05-10 23:19:21 -0400

    Added html parsing and weather bar

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 	<body>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-8">
+				<div class="col-md-7">
 					<div class="row">
 						<div class="col-md-12">
 							<div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -93,70 +93,55 @@
 					</div> <!-- end twitter feed -->
 				</div> <!-- end left col -->
 
-				<div class="col-md-4">
+				<div class="col-md-5">
 					<div class="row">
 						<img src="img/ttc-logo.png" width="50%"/>
 						<br />
 						<?php echo date("D M n") . " <span id=\"timer\"></span>"; ?>
 					</div> <!-- end time and date -->
-					<div class="row">  
-					<div id="plemx-root"></div> 
-						<a href="http://www.theweathernetwork.com">The Weather Network</a>
-						<script type="text/javascript"> 
-						  var _plm = _plm || [];
-						  _plm.push(['_btn', 30504]); 
-						  _plm.push(['_loc','caon0696']);
-						  _plm.push(['location', document.location.host ]);
-
-						  (function(d,e,i) {
-						  if (d.getElementById(i)) return;
-						  var px = d.createElement(e);
-						  px.type = 'text/javascript';
-						  px.async = true;
-						  px.id = i;
-						  px.src = ('https:' == d.location.protocol ? 'https:' : 'http:') + '//widget.twnmm.com/js/btn/pelm.js?orig=en_ca';
-						  var s = d.getElementsByTagName('script')[0];
-
-						  var py = d.createElement('link');
-						  py.rel = 'stylesheet'
-						  py.href = ('https:' == d.location.protocol ? 'https:' : 'http:') + '//widget.twnmm.com/styles/btn/styles.css'
-
-						  s.parentNode.insertBefore(px, s);
-						  s.parentNode.insertBefore(py, s);
-						})(document, 'script', 'plmxbtn');</script></div>
-					<div class="row">News</div>
+					<div class="row"> <!-- begin Forcast -->  
+					<iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" min-width="500" src="http://forecast.io/embed/#lat=42.3583&lon=-71.0603&name=Toronto&color=#00aaff&font=Georgia&units=ca"> </iframe>
+					</div> <!-- end Forcast -->
 					<div class="row">
+						News
+					</div> <!-- end news -->
+					<div class="row scorecard"> <!-- Begin scorecard -->  
 						<?php
 							$url = "https://www.ttc.ca/Customer_Service/Daily_Customer_Service_Report/index.jsp";
 							libxml_use_internal_errors(true);
 							$html=file_get_contents($url);
-							
-							$dom = new DOMDocument;
+							$dom = new \DOMDocument;
 							$dom->loadHTML($html);
-							
-							$table = $dom->getElementsByTagName('tbody');
 
-							$rows = $table->getElementsByTagName('tr');
-							$cols = $rows->getElementsByTagName('td');
-
-							$key = $cols->item(0)->nodeValue; // string(3) "key"
-							$val = $cols->item(1)->nodeValue;
-
-							foreach ($dom->getElementsByTagName('tr') as $node) {
-							//echo $node->getAttribute( 'tr' ); 
-							   //echo $dom->saveHtml($node), PHP_EOL;
-							   //echo "hmmm";
+							$Header = $dom->getElementsByTagName('th');
+							$Detail = $dom->getElementsByTagName('td');
+							foreach($Header as $NodeHeader) 
+							{
+								$tableData[] = trim($NodeHeader->textContent);
 							}
-							echo $key;
-							echo $val;
+							$i = 0;
+							$j = 0;
+							foreach($Detail as $sNodeDetail) 
+							{
+								//echo $sNodeDetail;
+								$tableDetail[$j][] = trim($sNodeDetail->textContent);
+								$i = $i + 1;
+								$j = $i % count($tableData) == 0 ? $j + 1 : $j;
+							}
+
+							for($i = 0; $i < 4; $i++)
+							{
+								?>
+								<p> Line : <?php print_r ($tableDetail[$i][1]); ?> </p>
+								<p> Target : <?php print_r ($tableDetail[$i][3]); ?> </p>
+								<p> Actual : <?php print_r ($tableDetail[$i][4]); ?> </p>
+								<?php
+							}
+
 						?>
-						ending
-					</div>
-						})(document, 'script', 'plmxbtn');</script>
-					</div> <!-- end weather -->
-					<div class="row">
-						News
-					</div> <!-- end news -->
+						</div>
+					</div> <!-- End scorecard -->  
+					
 					<div class="row"></div>
 				</div>
 			</div>
